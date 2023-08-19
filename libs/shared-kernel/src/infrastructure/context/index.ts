e35113f1780bb01ef7d5baa9096d2918768f681d
ola <raymond.oyinlola@gmail.com>
2023-08-19 14:19:41 +0800

    remove context manager

--- libs/shared-kernel/src/infrastructure/context/index.ts
+++ libs/shared-kernel/src/infrastructure/context/index.ts
@@ -1,4 +1,3 @@
-export * from './context-service.interface';
-export * from './context';
-export * from './context.service';
-export * from './context-manager';
+export * from "./context-service.interface";
+export * from "./context";
+export * from "./context.service";
