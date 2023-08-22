0c2776618f6f749445b91359fe7e6167b092cb4b
ola <raymond.oyinlola@gmail.com>
2023-08-22 09:11:11 +0800

    Add usermodule to auth and book modules

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { Module } from "@nestjs/common";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
+import { TYPES } from "apps/book/src/constants/contstants";
+import { UserRepository } from "./schema/user.repository";
+import { MongooseModule } from "@nestjs/mongoose";
+import { UserDataModel, UserSchema } from "./schema/user.schema";
+import { UserMapper } from "./user.mapper";
+import { AuditMapper } from "@app/shared-kernel/application/audit";
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: UserDataModel.name, schema: UserSchema }])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, { provide: TYPES.IUserRepository, useClass: UserRepository }, UserMapper, AuditMapper],
 })
 export class UsersModule {}
--- apps/book/src/book.module.ts
+++ apps/book/src/book.module.ts
@@ -8,10 +8,28 @@ import { BookDataModel, BookSchema } from "./schema/book.schema";
 import { BookMapper } from "./book.mapper";
 import { AuditMapper } from "@app/shared-kernel/application/audit";
 import { TYPES } from "./constants/contstants";
+import { AuthModule } from "apps/auth/src/auth.module";
+import { UserRepository } from "apps/auth/src/users/schema/user.repository";
+import { UserDataModel, UserSchema } from "apps/auth/src/users/schema/user.schema";
+import { UserMapper } from "apps/auth/src/users/user.mapper";
 
 @Module({
-  imports: [DatabaseModule, MongooseModule.forFeature([{ name: BookDataModel.name, schema: BookSchema }])],
+  imports: [
+    DatabaseModule,
+    MongooseModule.forFeature([
+      { name: BookDataModel.name, schema: BookSchema },
+      { name: UserDataModel.name, schema: UserSchema },
+    ]),
+    AuthModule,
+  ],
   controllers: [BookController],
-  providers: [BookService, { provide: TYPES.IBookRepoitory, useClass: BookRepository }, BookMapper, AuditMapper],
+  providers: [
+    BookService,
+    { provide: TYPES.IBookRepoitory, useClass: BookRepository },
+    { provide: TYPES.IUserRepository, useClass: UserRepository },
+    BookMapper,
+    AuditMapper,
+    UserMapper,
+  ],
 })
 export class BookModule {}
