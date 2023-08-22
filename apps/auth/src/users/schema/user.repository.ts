34441f0e01a7bcca166cd71ffc26ad36851fc5ea
ola <raymond.oyinlola@gmail.com>
2023-08-22 08:47:40 +0800

    create the user repository

--- apps/auth/src/users/schema/user.repository.ts
+++ apps/auth/src/users/schema/user.repository.ts
@@ -1,7 +1,21 @@
-import { GenericDocumentRepository } from '@app/shared-kernel/infrastructure/database/generic-document.repository';
-import { Injectable } from '@nestjs/common';
-import {  UserDocument } from './user.schema';
-import { User } from '../user';
+import { GenericDocumentRepository } from "@app/shared-kernel/infrastructure/database/generic-document.repository";
+import { Injectable } from "@nestjs/common";
+import { UserDataModel, UserDocument } from "./user.schema";
+import { User } from "../user";
+import { IUserRepository } from "../dto/user-repository.interface";
+import { UserMapper } from "../user.mapper";
+import { InjectConnection, InjectModel } from "@nestjs/mongoose";
+import { Connection, Model } from "mongoose";
 
 @Injectable()
-export class UserRepository extends GenericDocumentRepository<User, UserDocument>
+export class UserRepository extends GenericDocumentRepository<User, UserDocument> implements IUserRepository {
+  _userMapper: UserMapper;
+  constructor(
+    @InjectModel(UserDataModel.name) readonly userDataModel: Model<UserDocument>,
+    @InjectConnection() readonly connection: Connection,
+    userMapper: UserMapper
+  ) {
+    super(userDataModel, connection, userMapper);
+    this._userMapper = userMapper;
+  }
+}
